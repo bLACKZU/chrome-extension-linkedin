ce329d709ac1232887efc1eb5c9447abe6a5ed32
blackzu <satyakighosh65@gmail.com>
2023-02-23 17:40:53 +0530

    added code

---
+++ content.js
@@ -0,0 +1,10 @@
+
+    
+var connection = document
+.querySelectorAll(
+'button[class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view full-width"]');
+    
+for(var i = 0; i < connection.length; i = i + 1)
+    connection[i].click();
+
+// document.body.appendChild(connection); 
